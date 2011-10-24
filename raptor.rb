e2e57304ba0b71073d580af5d6179dc9a0bd8a69
Andrey Lisin <andrey.lisin@gmail.com>
2011-10-24 20:46:25 +0700

    Command line option parsing support was added.

---
+++ raptor.rb
@@ -0,0 +1,27 @@
+# The main script.
+
+require 'xml2fs'
+require 'optparse'
+
+def parse_options
+  options = {} # here optparse will store the arguments
+  optparse = OptionParser.new do |opts|
+    opts.on('-h', '--help', 'Display this screen') do
+      puts opts
+      exit
+    end
+    opts.on('-p', '--prefix PREFIX', 'path to repository') do |p|
+      options[:prefix] = p
+    end
+    opts.on('-s', '--scheme SCHEME', 'repository scheme') do |s|
+      options[:scheme] = s
+    end
+  end
+  optparse.parse!
+  options # return options
+end
+
+
+
+ 
+ 
